0a3505d29e4681b94cac129c3524d331c1264fde
Rathapong <dev01tcm@outlook.com>
2021-10-14 17:44:15 +0700

    start redirect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+// import App from './App'
 import Orders from './Orders';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     <Orders/>
   </React.StrictMode>,
   document.getElementById('root')
